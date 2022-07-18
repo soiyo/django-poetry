ea6392a46c98d334029ec4898070c0fc4e5ac919
soiyo <soiyosauce@gmail.com>
2022-07-18 18:32:25 +0900

    initial commit

---
+++ django_poetry/urls.py
@@ -0,0 +1,32 @@
+"""django_poetry URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from ninja import NinjaAPI
+
+# NinjaAPI를 인스턴스화
+api = NinjaAPI()
+
+# view 함수를 감싸 url을 넘겨줌
+@api.get("/add")
+def add(request, a: int, b: int):
+    return {"result": a + b}
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", api.urls),
+]
