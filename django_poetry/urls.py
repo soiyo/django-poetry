0c4f9e2f3203e076bebbbfa396a14638a8c17438
soiyo <soiyosauce@gmail.com>
2022-07-18 19:21:26 +0900

    isort 설치

--- django_poetry/urls.py
+++ django_poetry/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from typing import Dict
-from django.http import HttpRequest
 
 from django.contrib import admin
+from django.http import HttpRequest
 from django.urls import path
 from ninja import NinjaAPI
 
